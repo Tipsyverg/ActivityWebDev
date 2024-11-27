0c7bfe1fd13a4b700ec25b6554555b82a8c68db9
Tipsyverg <vergararupert@gmail.com>
2024-11-27 21:48:28 +0800

    done peysapp

--- PeysApp/peysapp.php
+++ PeysApp/peysapp.php
@@ -6,6 +6,33 @@
     <title>Peys App</title>
 </head>
 <body>
-    
+<?php
+// Set default values
+$size = isset($_POST['size']) && is_numeric($_POST['size']) ? intval($_POST['size']) : 60; 
+$borderColor = isset($_POST['borderColor']) ? $_POST['borderColor'] : '#000000';  
+
+$canvasWidth = $size;
+$canvasHeight = $size;
+?>
+
+<form id="settingsForm" method="post">
+    <label for="size">Select Photo Size:</label>
+    <input type="range" id="sizeSlider" name="size" min="10" max="100" value="<?php echo htmlspecialchars($size); ?>" step="10" oninput="this.nextElementSibling.value = this.value">
+    <output><?php echo htmlspecialchars($size); ?></output>
+    <br><br>
+
+    <label for="borderColor">Select Border Color:</label>
+    <input type="color" id="borderColor" name="borderColor" value="<?php echo htmlspecialchars($borderColor); ?>">
+    <br><br>
+
+    <button type="submit" name="submit">Process</button>
+    <br><br>
+
+    <img id="sourceImage" src="images.jfif" alt="Image for Canvas" hidden>
+
+    <div style="border:3px solid <?php echo htmlspecialchars($borderColor); ?>; width: <?php echo $canvasWidth; ?>px; height: <?php echo $canvasHeight; ?>px; overflow:hidden;">
+        <img src="images.jfif" alt="Image for Canvas" width="<?php echo $canvasWidth; ?>" height="<?php echo $canvasHeight; ?>">
+    </div>
+</form>
 </body>
 </html>
